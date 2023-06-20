04d0afa696eba0b0facd7826987f43b198b2ade3
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-06-20 17:11:06 +0200

    wip

---
+++ src/FoundationServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace QRFeedz\Foundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class FoundationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+}
