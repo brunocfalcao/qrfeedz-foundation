b33b8e4c2f93b709f9db6a50e11f35ab5e7df0e4
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-11 14:56:42 +0200

    Update QRFeedzResource.php

--- src/Abstracts/QRFeedzResource.php
+++ src/Abstracts/QRFeedzResource.php
@@ -4,21 +4,36 @@ namespace QRFeedz\Foundation\Abstracts;
 
 use App\Nova\Resource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use QRFeedz\Admin\Fields\HumanDateTime;
 
 abstract class QRFeedzResource extends Resource
 {
+    /**
+     * The current logged user.
+     *
+     * @return Authenticable
+     */
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    /**
+     * To be used within a panel, to show the 3 timestamp fields, correctly
+     * formatted and with view and authorization logic.
+     *
+     * @return array
+     */
     protected function timestamps(Request $request)
     {
         return [
-
             HumanDateTime::make('Created At'),
 
             HumanDateTime::make('Updated At'),
 
             HumanDateTime::make('Deleted At')
                          ->canSee(fn () => ! $request->findModel()->deleted_at == null),
-
         ];
     }
 }
