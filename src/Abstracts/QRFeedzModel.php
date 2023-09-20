44982d5dbd763cc6038feeee3a51157f9cbd9d7f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-21 00:11:35 +0200

    wip

--- src/Abstracts/QRFeedzModel.php
+++ src/Abstracts/QRFeedzModel.php
@@ -3,8 +3,14 @@
 namespace QRFeedz\Foundation\Abstracts;
 
 use Illuminate\Database\Eloquent\Model;
+use QRFeedz\Foundation\Classes\CustomEloquentQueryBuilder;
 
 abstract class QRFeedzModel extends Model
 {
     protected $guarded = [];
+
+    public function newEloquentBuilder($query)
+    {
+        return new CustomEloquentQueryBuilder($query);
+    }
 }
