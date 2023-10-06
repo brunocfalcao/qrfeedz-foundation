ef8cacbcd0528d3d35bf0dc07cb37c54143c060e
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-10-07 00:48:14 +0200

    Update QRFeedzResource.php

--- src/Abstracts/QRFeedzResource.php
+++ src/Abstracts/QRFeedzResource.php
@@ -6,9 +6,12 @@ use App\Nova\Resource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use QRFeedz\Admin\Fields\QRDateTime;
+use Titasgailius\SearchRelations\SearchesRelations;
 
 abstract class QRFeedzResource extends Resource
 {
+    use SearchesRelations;
+
     public static function softDeletes()
     {
         return false;
